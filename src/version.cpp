09a05960ced981f05dd11462ebda17e8f1cb56da
Anthony <5298658+thorn79@users.noreply.github.com>
2018-10-10 08:07:50 +1300

    Update version.cpp
    Update GIT commit ID and Date

--- src/version.cpp
+++ src/version.cpp
@@ -36,8 +36,8 @@ const std::string CLIENT_NAME("Ember");
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives.
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#    define GIT_COMMIT_ID "v1.1.5.3" // More informative with version number at this time.
-#    define GIT_COMMIT_DATE "Nov 30, 2017"
+#    define GIT_COMMIT_ID "v1.1.5.4" // More informative with version number at this time.
+#    define GIT_COMMIT_DATE "Oct 10, 2018"
 #endif
 
 #define BUILD_DESC_FROM_COMMIT(maj,min,rev,build,commit) \
